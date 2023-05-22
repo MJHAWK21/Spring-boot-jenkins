217bc89960a6c5f49570e1e249cb62579b68c7db
Meera javad <meerajavad@gmail.com>
2023-05-22 09:54:42 +0530

    Third COmmit

---
+++ src/main/java/com/web/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.web.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+
+    @GetMapping("/he")
+    public String hello() {
+        return "Hello";
+    }
+
+}
